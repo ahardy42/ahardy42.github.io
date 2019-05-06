12c47fd5a0c44b4777be6c44091b6b36edc0bb0b
andy_hardy <aohardy@gmail.com>
2019-05-06 12:47:12 -0600

    beginning the one page shift

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
     $(".header, .container").animate({opacity: "1"}, 2000);
+
+    // $("body").on("wheel", function(event) {
+    //   event.preventDefault();
+    //   console.log("delta Y", event.originalEvent.deltaY);
+    //   var deltaY = event.originalEvent.deltaY;
+    //   if (deltaY > 5) { // sets a threshold for the scrolling event
+    //     window.location = "portfolio.html";
+    //   }
+    // });
   
   });
