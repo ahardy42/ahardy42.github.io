aa8f78f3b0d1b314e6e29b18b1694f715cc61275
andy_hardy <aohardy@gmail.com>
2019-05-09 17:16:07 -0600

    shoulda commit yesterday

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,14 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(".header, .container").animate({opacity: "1"}, 2000);
+  $(".header, .card_face, .scene").animate({ opacity: "1" }, 1);
 
-    // $("body").on("wheel", function(event) {
-    //   event.preventDefault();
-    //   console.log("delta Y", event.originalEvent.deltaY);
-    //   var deltaY = event.originalEvent.deltaY;
-    //   if (deltaY > 5) { // sets a threshold for the scrolling event
-    //     window.location = "portfolio.html";
-    //   }
-    // });
+  $(".card").on("click", function() {
+    console.log("you clicked me");
+    // change the css to flip the card
+    var card = $(".card");
+    
+    if (card.attr("style") === "transform: rotateX(180deg);") {
+      card.css("transform", "rotateX(360deg)");
+      card.removeAttr("style");
+    } else {
+      card.css("transform", "rotateX(180deg)");
+    }
+    console.log(card.attr("style"));
+  })
+  // ----------------- functions ------------------------------
+  // function to break down the container section resulting from onclick/onswipe 
   
-  });
+
+  // function to build the container section again resulting from onclick/onswipe
+
+
+  // onclick function for the arrow 
+});
